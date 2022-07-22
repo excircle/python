35dd3d9c78efc780392cf5cff9d057c6c1d2a99e
Alexander Kalaj <akalaj@hashicorp.com>
2022-07-21 17:16:01 -0700

    Add gen username and find digits code

--- challenges/find-usernames-with-numbers/find-nums-in-username.py
+++ challenges/find-usernames-with-numbers/find-nums-in-username.py
@@ -2,12 +2,8 @@ if __name__ == '__main__':
 	with open('usernames.txt', 'r') as FILE:
 		users_with_numbers = []
 		for name in FILE.read().splitlines():
-			split_list = list(name)
-			for char in split_list:
-				if char.isdigit():
-					users_with_numbers.append(name)
-					break
-				else:
-					pass
+			digit_analysis = [char.isdigit() for char in name]
+			if True in set(digit_analysis):
+				users_with_numbers.append(name)
 		print(users_with_numbers)
 		print(len(users_with_numbers))
