a8156426ace97e4d06347b395046edfd2e67f070
Alexander Kalaj <unixdisciple.us@gmail.com>
2024-12-26 11:43:34 -0800

    Solution for the bon appetit challenge

--- challenges/bon-appetit/bon.py
+++ challenges/bon-appetit/bon.py
@@ -1,8 +1,16 @@
 def bonAppetit(bill, k, b):
+    anna_share = sum( [ x for x in bill if x != bill[k] ] ) // 2
+    if b == anna_share:
+        print("bon appetit")
+    else:
+        print( b - anna_share)
     
 
 def main():
-    return
+    bill = [3, 10, 2, 9]
+    skip_index = 1
+    annas_contribution = 12
+    bonAppetit(bill, skip_index, annas_contribution)
 
 if __name__ == '__main__':
     main()
